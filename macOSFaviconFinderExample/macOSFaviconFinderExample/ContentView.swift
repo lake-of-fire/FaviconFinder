a042fb20940611c1d00a3bba8349dffc1c552765
Alex Ehlke <alex.ehlke@gmail.com>
2026-08-29 20:19:52 -0400

    Restrict example view state

--- macOSFaviconFinderExample/macOSFaviconFinderExample/ContentView.swift
+++ macOSFaviconFinderExample/macOSFaviconFinderExample/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
 
-    @State var urlStr = "https://www.plinky.app"
+    @State private var urlStr = "https://www.plinky.app"
     @ObservedObject var imageLoader = ImageLoader()
 
     var body: some View {
